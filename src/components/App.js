8a7ce25628d717fef12bb60164b395d6e3864f4a
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-04-28 22:24:50 +0100

    Finished lesson 9 - created react router

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+
 class App extends React.Component {
   render() {
     return (
