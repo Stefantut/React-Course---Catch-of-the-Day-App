942b45c311e243dbfa46f5a78165d3fe517be863
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-04-28 21:36:46 +0100

    Finished lesson 7 - pass data with props

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Inventory />
         <Order />
