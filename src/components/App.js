2e64e7c90acd8970c720471511f4056fa7b364c6
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-05-02 23:02:18 +0100

    Created AddFish Component

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory />
       </div>
     );
   }
