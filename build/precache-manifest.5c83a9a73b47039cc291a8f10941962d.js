7ad8c097d49961bfbda260cfd68fa1b1b226dd2d
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-05-08 22:20:10 +0100

    Finished lesson 25 - Building React for Production

---
+++ build/precache-manifest.5c83a9a73b47039cc291a8f10941962d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d649122f430cdbb0b5970531c3f299d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3161d1b73eaa47be375a",
+    "url": "/static/css/main.53756eb6.chunk.css"
+  },
+  {
+    "revision": "292766c5be6e859ed13f",
+    "url": "/static/js/2.caa0e871.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.caa0e871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3161d1b73eaa47be375a",
+    "url": "/static/js/main.ecb8466c.chunk.js"
+  },
+  {
+    "revision": "bff35b08683e7987d07c",
+    "url": "/static/js/runtime-main.152ba60f.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
